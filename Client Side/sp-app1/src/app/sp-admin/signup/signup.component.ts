52c38c9b66d03461f939320e179b8d2cebaa7729
Akash Chaurasiya <akashchaurasia617@gmail.com>
2023-04-16 23:45:52 +0530

    client side change2

--- Client Side/sp-app1/src/app/sp-admin/signup/signup.component.ts
+++ Client Side/sp-app1/src/app/sp-admin/signup/signup.component.ts
@@ -13,12 +13,12 @@ export class SignupComponent {
   constructor(private service: SignupService, private formBuider: FormBuilder){
     this.owner = formBuider.group({
       id: new FormControl('', Validators.required),
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      mobileNumber: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl(),
-      userType: new FormControl()
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      mobileNumber: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      userType: new FormControl('', Validators.required)
     });
   }
 
@@ -28,9 +28,28 @@ export class SignupComponent {
   get firstName(){
     return this.owner.get('firstName');
   }
+  get lastName(){
+    return this.owner.get('lastName');
+  }
+  get mobileNumber(){
+    return this.owner.get('mobileNumber');
+  }
+  get email(){
+    return this.owner.get('email');
+  }
+  get password(){
+    return this.owner.get('password');
+  }
+  get userType(){
+    return this.owner.get('userType');
+  }
   save(){
     // console.log(this.owner.value);
     // console.log(this.owner.value.userType);
+    if(this.owner.invalid){
+      this.owner.markAllAsTouched();
+      return;
+    }
     this.service.doSignup(this.owner).subscribe(
       r1 => {
         console.log(r1);
